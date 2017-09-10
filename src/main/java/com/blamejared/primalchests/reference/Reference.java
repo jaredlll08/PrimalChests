152e011d76701c085e7a0e0219b6f06f73e2d476
Jared <jaredlll08@gmail.com>
2017-09-11 01:36:00 +0200

    version push

--- src/main/java/com/blamejared/primalchests/reference/Reference.java
+++ src/main/java/com/blamejared/primalchests/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
     
     public static final String MOD_ID = "primalchests";
     public static final String MOD_NAME = "PrimalChests";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     
 
 }
