22f9eaff10bb39516315a1b080cfd815fa02ae4b
SteamPunkNation <steampunknation.ad@gmail.com>
2022-02-16 08:39:01 -0500

    Package naming fixed
    Updated information, package name, jar name, etc

---
+++ src/main/java/com/blamejared/primalchests/client/gui/screen/AdvancedPrimalChestScreen.java
@@ -0,0 +1,54 @@
+package com.blamejared.primalchests.client.gui.screen;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.blamejared.primalchests.PrimalChests;
+import com.blamejared.primalchests.common.container.AdvancedPrimalChestContainer;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class AdvancedPrimalChestScreen extends ContainerScreen<AdvancedPrimalChestContainer> {
+
+    private static final ResourceLocation ADVANCED_PRIMAL_CHEST_GUI = new ResourceLocation(PrimalChests.MOD_ID,
+            "textures/gui/advanced_primal_chest.png");
+
+    public AdvancedPrimalChestScreen(AdvancedPrimalChestContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
+        super(screenContainer, inv, titleIn);
+
+        this.leftPos = 0;
+        this.topPos = 0;
+        this.imageWidth = 176;
+        this.imageHeight = 166;
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(matrixStack);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        this.renderTooltip(matrixStack, mouseX, mouseY);
+    }
+
+    @Override
+    protected void renderLabels(MatrixStack matrixStack, int x, int y) {
+        this.font.draw(matrixStack, this.inventory.getDisplayName(),
+                (float)this.inventoryLabelX, (float)this.inventoryLabelY, 4210752);
+        this.font.draw(matrixStack, this.title,
+                (float) this.inventoryLabelX, (float) this.inventoryLabelY - 64, 4210752);
+
+    }
+
+    @Override
+    protected void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
+        RenderSystem.color4f(1f,1f,1f,1f);
+        this.minecraft.textureManager.bind(ADVANCED_PRIMAL_CHEST_GUI);
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
+        this.blit(matrixStack, x, y, 0,0, this.imageWidth, this.imageHeight);
+    }
+
+}
---
+++ src/main/java/com/blamejared/primalchests/common/items/ItemRegister.java
@@ -0,0 +1,19 @@
+package com.blamejared.primalchests.common.items;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.blamejared.primalchests.PrimalChests.MOD_ID;
+
+public class ItemRegister {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
+
+    //Items
+
+
+    //Block Items (Block Drops)
+
+}
